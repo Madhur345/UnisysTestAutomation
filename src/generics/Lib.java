e1d5d711a742fa8101ddc56f00c7a4ae96f2d1b1
Madhur345 <38024131+Madhur345@users.noreply.github.com>
2019-11-23 07:44:47 +0530

    Vishal

--- src/generics/Lib.java
+++ src/generics/Lib.java
@@ -52,7 +52,6 @@ public class Lib implements IAutoConstant{
 		try {
 			Date d = new Date();
 			String currentDate = d.toString().replaceAll(":","_");
-			//String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
 			extent=new ExtentReports(System.getProperty("user.dir")+"/ExtentReports/"+"InteliServe Test Automation Report" +"_"+ currentDate + ".html", true);
 			extent.loadConfig(new File(System.getProperty("user.dir")+"/testdata/ReportsConfig.xml")); 
 			extent.addSystemInfo("Selenium Version", "3.7.1").addSystemInfo(
